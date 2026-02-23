99752019362f2af912955cf18c6e7ffce8f43de7
Mukta Mehra <2muktamehra@gmail.com>
2026-02-23 17:45:21 -0500

    Add React app with Header component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+
+function App() { // ← this is a React component
+  return (     //← everything inside here shows on the page
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App  // ← makes this component available to other files
